74ec128947416e6154cb33518605cc8c90f0f750
Ahmad Ataya <ahmad.3taya@gmail.com>
2018-07-18 02:56:34 +0300

    t

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,11 @@
 'use strict';
 var _ = require('lodash');
+var g = require('strong-globalize')();
+var debug = require('debug')('loopback:user');
+
 module.exports = function(User) {
 	User.validatesUniquenessOf('phoneNumber',{message: 'phoneNumber already exists'});
+	// user.validatesLengthOf('phoneNumber', {min: 9,max: 10 message: {min: 'phoneNumber is not syrian Number'}});
 	// User.validatesNumericalityOf('phoneNumber');
 	User.validatesInclusionOf('gender', {in: ['male', 'female']});
 	User.validatesInclusionOf('status', {in: ['pending', 'activated','deactivated']});
@@ -12,6 +16,92 @@ module.exports = function(User) {
 	// _.each(functionsDisable,(f)=>{
 	// 	User.disableRemoteMethod(f, false);
 	// })
+
+	User.beforeRemote('create', function(ctx, modelInstance, next) {
+		ctx.req.body.phoneNumber = Number(ctx.req.body.phoneNumber)
+	    next();
+	});
+
+	User.login = function(credentials, include, fn){
+		var self = this;
+	    if (typeof include === 'function') {
+	      fn = include;
+	      include = undefined;
+	    }
+
+	    fn = fn || utils.createPromiseCallback();
+
+	    include = (include || '');
+	    if (Array.isArray(include)) {
+	      include = include.map(function(val) {
+	        return val.toLowerCase();
+	      });
+	    } else {
+	      include = include.toLowerCase();
+	    }
+
+
+	    var query = {
+	    	phoneNumber : Number(credentials.phoneNumber)	    
+	    }
+	    
+	    if (!query.phoneNumber) {
+	      var err2 = new Error(g.f('{{phoneNumber}} is required'));
+	      err2.statusCode = 400;
+	      err2.code = 'PHONENUMBER_EMAIL_REQUIRED';
+	      fn(err2);
+	      return fn.promise;
+	    }
+
+	    self.findOne({where: query}, function(err, user) {
+	    	console.log(query,err,user);
+	      var defaultError = new Error(g.f('login failed'));
+	      defaultError.statusCode = 401;
+	      defaultError.code = 'LOGIN_FAILED';
+
+	      function tokenHandler(err, token) {
+	        if (err) return fn(err);
+	        if (Array.isArray(include) ? include.indexOf('user') !== -1 : include === 'user') {
+	          // NOTE(bajtos) We can't set token.user here:
+	          //  1. token.user already exists, it's a function injected by
+	          //     "AccessToken belongsTo User" relation
+	          //  2. ModelBaseClass.toJSON() ignores own properties, thus
+	          //     the value won't be included in the HTTP response
+	          // See also loopback#161 and loopback#162
+	          token.__data.user = user;
+	        }
+	        fn(err, token);
+	      }
+
+	      if (err) {
+	        debug('An error is reported from User.findOne: %j', err);
+	        fn(defaultError);
+	      } else if (user) {
+	        user.hasPassword(credentials.password, function(err, isMatch) {
+	          if (err) {
+	            debug('An error is reported from User.hasPassword: %j', err);
+	            fn(defaultError);
+	          } else if (isMatch) {
+	            
+	            if (user.createAccessToken.length === 2) {
+	                user.createAccessToken(credentials.ttl, tokenHandler);
+	            } else {
+	                user.createAccessToken(credentials.ttl, credentials, tokenHandler);
+	            }
+	            
+	          } else {
+	            debug('The password is invalid for user %s', query.email || query.username);
+	            fn(defaultError);
+	          }
+	        });
+	      } else {
+	        debug('No matching record is found for user %s', query.email || query.username);
+	        fn(defaultError);
+	      }
+	    });
+	    return fn.promise;
+	}
+
 
 
 	User.afterRemote('*.__link__roles', function(context, role, next) {
