a7c979bfd808c8096a127dea9ea0d7c40148dde6
Rami Al-Zebak <17zrami@gmail.com>
2019-03-18 14:52:57 +0200

    warehouse entities

---
+++ server/models/warehouse-products.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function(Warehouseproducts) {
+
+    Warehouseproducts.validatesInclusionOf('status', {
+        in: ['available', 'unavailable', 'pending']
+    });
+
+    
+
+};
