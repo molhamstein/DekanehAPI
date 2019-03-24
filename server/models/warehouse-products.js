88c12affcf24a8a745e9572d08f0897074f51cfe
Rami Al-Zebak <17zrami@gmail.com>
2019-03-24 13:25:10 +0200

    validate uniqueness of WarehouseProduct

--- server/models/warehouse-products.js
+++ server/models/warehouse-products.js
@@ -1,25 +1,38 @@
 'use strict';
 
-module.exports = function(Warehouseproducts) {
+module.exports = function (Warehouseproducts) {
 
-   
+
 
     Warehouseproducts.validatesInclusionOf('status', {
         in: ['available', 'unavailable', 'pending']
     });
-            
-    Warehouseproducts.prototype.updateExpectedCount = function (expectedCountDiff){
-        
-         this.expectedCount = this.expectedCount + expectedCountDiff; 
-        return this.save(); 
+
+    Warehouseproducts.prototype.updateExpectedCount = function (expectedCountDiff) {
+
+        this.expectedCount = this.expectedCount + expectedCountDiff;
+        return this.save();
     }
-    Warehouseproducts.prototype.updateEffictiveCount = function(effictiveCountDiff){
-        this.effictiveCount = this.effictiveCount + effictiveCountDiff; 
-        return this.save(); 
+    Warehouseproducts.prototype.updateEffictiveCount = function (effictiveCountDiff) {
+        this.effictiveCount = this.effictiveCount + effictiveCountDiff;
+        return this.save();
     }
 
-    
-    
+
+    Warehouseproducts.beforeRemote('create', (ctx, modelInstance, next) => {
+
+        // @todo unique index of { warehouseId, productAbstractId }
+        // validate uniqueness of Warehouse Product         
+        let { warehouseId, productAbstractId } = ctx.req.body;
+        Warehouseproducts.findOne({ where: { warehouseId, productAbstractId } }, (err, warehouseProduct) => {
+            if (err || warehouseProduct) {
+                next(ERROR(422, "Warehouse Product Already Exist"));
+            } else {
+                next();
+            }
+        });
+    });
+
 
 };
 
