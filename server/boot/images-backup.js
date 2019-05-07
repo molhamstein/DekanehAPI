7a74d4fe80fca8856223083e52f77d0b4702345d
Rami Al-Zebak <17zrami@gmail.com>
2019-05-07 16:44:10 +0300

    images backup

---
+++ server/boot/images-backup.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = async function (app) {
+
+
+    let fs = require('fs');
+    let _ = require('lodash');
+    let exec = require('child_process').exec;
+    let CronJob = require('cron').CronJob;
+    let tar = require('tar');
+    let path = require("path");
+
+    let options = {
+
+        sourcePath: "files/images/",
+        backupPath: "../images-backup/"
+
+    };
+
+    async function imagesBackup() {
+
+        console.log("images backup");
+        let images = await fs.promises.readdir(options.sourcePath);
+
+        for (let image of images) {
+
+            let imageSourcePath = path.join(options.sourcePath, image);
+            let imageBackupPath = path.join(options.backupPath, image);
+
+            if (!fs.existsSync(imageBackupPath)) {
+                fs.copyFile(imageSourcePath, imageBackupPath, (err) => { });
+            }
+        }
+
+
+    }
+
+    // every midnight 
+    let job = new CronJob('0 0 0 * * *', function () {
+        imagesBackup();
+        job.stop();
+    }, null, true);
+
+}; 
