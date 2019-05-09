aaa16a18c0ec497efad39064550a5ed6942973a4
Rami Al-Zebak <17zrami@gmail.com>
2019-05-09 16:16:05 +0300

    product abstract prices migration

--- bin/proudct-abstract-prices-query.js
+++ bin/proudct-abstract-prices-query.js
@@ -1,36 +1,66 @@
-var col = db.getCollection('productAbstract').aggregate([
+var MongoClient = require('mongodb').MongoClient;
+let connectionString = "mongodb://localhost:27017/";
+let dbs = {
+    "current": "dekaneh_product_abstract_target",
+};
+let main = async () => {
 
-    {
-        $lookup: {
-            from: 'products',
-            localField: '_id',
-            foreignField: 'productAbstractId',
-            as: 'products'
+    let startTime = (new Date());
+    console.log("migrating product abstract");
+    let connection = await MongoClient.connect(connectionString, { useNewUrlParser: true });
+    let db = connection.db(dbs.current);
+
+    // migrating products 
+    console.log("migrating abstract products");
+
+    let warehouse = await db.collection('warehouse').findOne({});
+    for (let productAbstract of await db.collection('productAbstract').find({}).toArray()) {
+
+
+        let product = await db.collection("products").findOne({ productAbstractId: productAbstract._id });
+
+        if (!product) continue;
+
+
+        productAbstract.officialConsumerPrice = product.marketOfficialPrice;
+        productAbstract.officialMassMarketPrice = product.wholeSaleMarketPrice;
+
+        await db.collection('productAbstract').updateOne({ _id: productAbstract._id }, { $set: productAbstract });
+
+
+        let warehouseProduct = {
+
+            productAbstractId: productAbstract._id,
+            warehouseId: warehouse._id,
+            warningThreshold: 0,
+            avgSellingPrice: 0,
+            accumulatedSellingCountOverTime: 0,
+            avgBuyingPrice: 0,
+            accumulatedBuyingCountOverTime: 0,
+            threshold: 0,
+            status: "available",
+            expectedCount: 0,
+            totalCount: 0
         }
+
+        try{
+        await db.collection('warehouseProducts').insertOne(warehouseProduct); 
+        }catch(e){
+            
+        }
+
+
     }
 
-]);
-var bulks = [];
-col.forEach((x) => {
 
-    if (x.products[0]) {
-        bulks.push(
+    console.log("done migrating product abstract");
 
-            {
-                updateOne:
-                {
-                    "filter": { _id: x._id },
-                    "update": {
-                        $set:
-                        {
-                            officialConsumerPrice: x.products[0].marketOfficialPrice,
-                            officialMassMarketPrice: x.products[0].wholeSaleMarketPrice
-                        }
-                    }
-                }
-            }
+    connection.close();
 
-        );
-    }
+    let endTime = new Date();
+    let diff = endTime - startTime;
+    console.log(`Finished in ${diff}ms`);
 
-}); 
+};
+
+main();
