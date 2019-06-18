183e9ecd4fa09b2a3203cc0de510dfa5f231acae
Rami Al-Zebak <17zrami@gmail.com>
2019-06-18 18:39:03 +0300

    create award

--- server/models/award-action.js
+++ server/models/award-action.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(AwardAction) {
-
+    AwardAction.validatesInclusionOf('type', { in: ['price', 'count' ]});
 };
--- server/models/award.js
+++ server/models/award.js
@@ -1,5 +1,37 @@
 'use strict';
 
-module.exports = function(Award) {
+module.exports = function (Award) {
+
+
+
+
+    Award.afterRemote('create', async function (ctx, modelInstance) {
+
+
+
+
+        let { from, to, every } = modelInstance;
+
+        let nextEnd = (start, every) => {
+            let end = new Date(start);
+            end.setDate(end.getDate() + every - 1);
+            return end;
+        };
+
+
+        for (let start = new Date(from), end = nextEnd(start, every); end <= to; start = new Date(end), start.setDate(start.getDate() + 1), end = nextEnd(start, every)) {
+            
+            modelInstance.periodsRelation.create({
+                from: new Date(start),
+                to: new Date(end)
+            });
+        }
+
+      
+
+        
+
+    });
+
 
 };
